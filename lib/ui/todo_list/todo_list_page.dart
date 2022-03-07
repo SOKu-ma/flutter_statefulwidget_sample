a465868534e1af26bee3d87ff25b994fe463a9cc
SOKu-ma <sokumaeng2020@gmail.com>
2022-03-07 19:28:04 +0900

    TODOの入れ替え（ListView）、削除機能追加

--- lib/ui/todo_list/todo_list_page.dart
+++ lib/ui/todo_list/todo_list_page.dart
@@ -21,62 +21,77 @@ class _TodoListPageState extends State<TodoListPage> {
       appBar: AppBar(
         title: Text(Title),
       ),
-      body: ListView.builder(
-        itemCount: _todoListModel.todoListModel.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(_todoListModel.todoListModel[index].todoTitle),
-              trailing: Checkbox(
-                onChanged: (value) {
-                  setState(() {
-                    _todoListModel.todoListModel[index].isCheckd =
-                        _todoListModel.todoListModel[index].isCheckd
-                            ? false
-                            : true;
-                  });
-                },
-                value: _todoListModel.todoListModel[index].isCheckd,
+      body: ReorderableListView(
+        onReorder: ((oldIndex, newIndex) {
+          setState(() {
+            if (oldIndex < newIndex) {
+              newIndex -= 1;
+            }
+            final TodoModel _todo =
+                _todoListModel.todoListModel.removeAt(oldIndex);
+            _todoListModel.todoListModel.insert(newIndex, _todo);
+          });
+        }),
+        children: [
+          for (final todo in _todoListModel.todoListModel)
+            Dismissible(
+              key: UniqueKey(),
+              background: Container(
+                color: Colors.red,
+                child: Icon(Icons.delete),
               ),
-              onTap: () {
-                showDialog(
-                  context: context,
-                  barrierDismissible: false,
-                  builder: (_) {
-                    return AlertDialog(
-                      title: Text("TODOタスクを変更"),
-                      content: TextField(
-                        controller: _textFieldController,
-                        decoration: InputDecoration(
-                          hintText: "タスクを入力",
-                        ),
-                      ),
-                      actions: [
-                        ElevatedButton(
-                            child: Text("Cancel"),
-                            onPressed: () {
-                              _textFieldController.text = "";
-                              Navigator.pop(context);
-                            }),
-                        ElevatedButton(
-                          child: Text("OK"),
-                          onPressed: () {
-                            setState(() {
-                              _todoListModel.todoListModel[index].todoTitle =
-                                  _textFieldController.text;
-                              _textFieldController.text = "";
-                              Navigator.pop(context);
-                            });
-                          },
-                        ),
-                      ],
+              child: Card(
+                child: ListTile(
+                  title: Text(todo.todoTitle),
+                  trailing: Checkbox(
+                    onChanged: (value) {
+                      setState(() {
+                        todo.isCheckd = todo.isCheckd ? false : true;
+                      });
+                    },
+                    value: todo.isCheckd,
+                  ),
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (_) {
+                        return AlertDialog(
+                          title: Text("TODOタスクを変更"),
+                          content: TextField(
+                            controller: _textFieldController,
+                            decoration: InputDecoration(
+                              hintText: "タスクを入力",
+                            ),
+                          ),
+                          actions: [
+                            ElevatedButton(
+                                child: Text("Cancel"),
+                                onPressed: () {
+                                  _textFieldController.text = "";
+                                  Navigator.pop(context);
+                                }),
+                            ElevatedButton(
+                              child: Text("OK"),
+                              onPressed: () {
+                                setState(() {
+                                  if (_textFieldController.text != "") {
+                                    todo.todoTitle = _textFieldController.text;
+                                  }
+                                  _textFieldController.text = "";
+                                  Navigator.pop(context);
+                                });
+                              },
+                            ),
+                          ],
+                        );
+                      },
                     );
                   },
-                );
-              },
-            ),
-          );
-        },
+                ),
+              ),
+            )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
